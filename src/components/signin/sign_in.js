8f2298ac3a4fa5d3f627f7c03f92f442901882c9
Ramkumar <n171007@rguktn.ac.in>
2022-12-14 08:19:50 +0530

    signin changed

--- src/components/signin/sign_in.js
+++ src/components/signin/sign_in.js
@@ -1,28 +1,57 @@
 import React from 'react';
 // import Register from './registration';
+import { useState } from "react";
 import './signin.css'
 /**
  * page to sign into the website
  * @returns 
  */
 function Sign_in() {
+
+  const [uid, setUid] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleInput = (event) => {
+    switch (event.target.name) {
+        case "uid":
+            setUid(event.target.value)
+            console.log("handle input")
+            break;
+        case "password":
+            setPassword(event.target.value)
+            break;
+       
+        default:
+        // code block
+    }
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(`Submit Clicked ra bhai and value of uid is ${uid}`)
+    console.log(`Submit Clicked ra bhai and value of password is ${password}`)
+    // setformErrors(validatename(username, email, password, repeatPassword))
+    event.preventDefault()
+}
+
+
   return (
     <>
 
-      <div class="login-body"><h1>Login</h1>
-        <form class="form-signin">
-          <div class="row">
-            <label for="name">University Id:</label>
-            <input type="number" name="number" autocomplete="off" placeholder="N09XXXX" />
+      <div className="login-body"><h1>Login</h1>
+        <form className="form-signin">
+          <div className="row">
+            <label htmlFor="name">University Id:</label>
+            <input type="text" id="uid" name="uid"  placeholder="N09XXXX" onChange={handleInput}/>
           </div>
-          <div class="row">
-            <label for="password">Password</label>
-            <input type="password" name="password" />
+          <div className="row">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="name" name="password" onChange={handleInput} />
           </div>
-          <button type="submit">Login</button>
-          <p class="mb-0  text-center">Don't have an account? <a href={"#"}>Sign
+          <button type="submit" onClick={handleSubmit}>Login</button>
+          <p className="mb-0  text-center">Don't have an account? <a href={"#"}>Sign
             Up</a></p>
-          <p class="mb-0  text-center">Forgot Password? <a href="forget_password.html">Clich Here</a></p>
+          <p className="mb-0  text-center">Forgot Password? <a href="forget_password.html">Clich Here</a></p>
         </form>
       </div>
     </>
